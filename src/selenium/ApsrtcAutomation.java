05dc67344940672c78d6cce4199cdf9582d710bf
NagKothapalli <nag022@gmail.com>
2022-12-23 23:10:12 +0530

    Dec23rd code pushed

--- src/selenium/ApsrtcAutomation.java
+++ src/selenium/ApsrtcAutomation.java
@@ -1,5 +1,8 @@
 package selenium;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -12,6 +15,8 @@ import org.openqa.selenium.interactions.Actions;
 public class ApsrtcAutomation
 {
 	//org.openqa.selenium.UnhandledAlertException: unexpected alert open: {Alert text : Please select end place.}
+	//org.openqa.selenium.StaleElementReferenceException: stale element reference: element is not attached to the page document
+	//org.openqa.selenium.NoSuchSessionException: Session ID is null. Using WebDriver after calling quit()?
 	WebDriver driver;
 	public ApsrtcAutomation()
 	{
@@ -52,13 +57,62 @@ public class ApsrtcAutomation
 		actions.moveToElement(destination).click().sendKeys("GUNTUR").pause(1000).sendKeys(Keys.ENTER).build().perform();
 		//actions.pause(1000).sendKeys(Keys.ENTER).build().perform();
 		driver.findElement(By.xpath("//input[@id='txtJourneyDate']")).click();
-		WebElement jDate = driver.findElement(By.xpath("//div[@class='ui-datepicker-group ui-datepicker-group-first']//table//tbody//tr//a[text()='28']"));
+		WebElement jDate = driver.findElement(By.xpath("//div[@class='ui-datepicker-group ui-datepicker-group-first']//table//tbody//tr//a[text()='30']"));
 		jDate.click();
 		driver.findElement(By.xpath("//input[@name='searchBtn']")).click();
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("//div[@id='returnDiscountModal']//input[@name='searchBtn']")).click();
 		//jDate.click();
-		driver.findElement(By.xpath("//div[@class='ui-datepicker-group ui-datepicker-group-first']//table//tbody//tr//a[text()='28']")).click();
+		driver.findElement(By.xpath("//div[@class='ui-datepicker-group ui-datepicker-group-first']//table//tbody//tr//a[text()='30']")).click();
 	}
+	@Test
+	public void mouseOperations()
+	{
+		System.out.println("Test Case : Book Bus Ticket");
+		WebElement fromCity = driver.findElement(By.xpath("//input[@name='source']"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(fromCity).pause(1000).click().build().perform();//Mouse Hover , Click
+		actions.moveToElement(fromCity).pause(1000).sendKeys("HYDERABAD").build().perform(); ////Mouse Hover , Enter Text
+		actions.moveToElement(fromCity).pause(1000).doubleClick().build().perform(); ////Mouse Hover , Double Click
+		actions.moveToElement(fromCity).pause(1000).contextClick().build().perform();//Mouse Hover , Right Click
+	}
+	@Test
+	public void workWithMultipleWindows() throws InterruptedException
+	{
+		driver.findElement(By.xpath("//a[@title='TimeTable / Track']")).click();
+		Set<String> myset = driver.getWindowHandles();
+		System.out.println("Windows Count Before:" + myset.size());
+		driver.findElement(By.xpath("//a[text()='All services Time Table & Tracking']")).click();
+		myset = driver.getWindowHandles();
+		System.out.println("Windows Count After :" + myset.size());
+	    ArrayList<String> mylist = new ArrayList<String>(myset);
+	    for(int i=0;i<mylist.size();i++) {
+	    	System.out.println("Session ID :" + mylist.get(i));
+	    }
+	    System.out.println("Title of the first window :"+ driver.getTitle());
+	    driver.switchTo().window(mylist.get(1));
+	    System.out.println("Title of the second window :"+ driver.getTitle());
+	    //driver.switchTo().defaultContent();
+	    driver.close();
+	    //driver.quit();
+	    System.out.println(driver);
+	    driver.switchTo().window(mylist.get(0));
+	    Thread.sleep(3000);
+	    driver.findElement(By.xpath("//a[@title='Home']")).click();
+	    driver.quit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
